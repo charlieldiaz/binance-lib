ccec106b52a088be56346f926a06dc2a46ed8615
charlieldiaz <canutox182@gmail.com>
2023-04-04 20:17:30 -0700

    added jest library

---
+++ src/__tests__/tradeActions/cancelAllOpenOrders.test.ts
@@ -0,0 +1,34 @@
+import cancelAllOpenOrders from "../../tradeActions/cancelAllOpenOrders";
+
+test("Checks cancel All OpenOrders receives does not receive empty string", () => {
+  expect(cancelAllOpenOrders(symbol)).toBe(3);
+});
+
+const util = require("../util");
+const { fetchData } = require("./fetchData");
+
+require("dotenv").config();
+const secretKey = process.env.SECRET_KEY;
+const apiKey = process.env.API_KEY;
+
+async function cancelAllOpenOrders(symbol) {
+  const endPoint = "v1/allOpenOrders";
+  const params = util.sortParamsAlphabeticallyOmitEmptySignV({
+    symbol,
+    timestamp: Date.now(),
+  });
+  const signature = util.getSignature(params, secretKey);
+
+  let url = `${endPoint}?${params}&signature=${signature}`;
+
+  const requestOptions = {
+    headers: { "X-MBX-APIKEY": apiKey },
+    url,
+    method: "DELETE",
+  };
+
+  const rawNeqwOrderData = await fetchData(requestOptions, endPoint);
+  return rawNeqwOrderData;
+}
+
+module.exports = { cancelAllOpenOrders };
